f9add5c07d1bb8be421879760f87a295e0c47ab2
Sai Dinesh <saidineshbb24@gmail.com>
2022-11-26 20:03:20 +0530

    New commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f5d981b2efmsh4841079cd284d20p1d9ef1jsn8351970ca966',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="d3a27a08c1a908e13f59c43dfb6378d3";
